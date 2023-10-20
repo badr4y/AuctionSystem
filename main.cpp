bf283d1d1be8b5af6168bf7d0144a5f659fb2628
Adelite25 <mahmoud.mokrane25@gmail.com>
2023-10-20 15:57:32 +0200

    création des fichier Item.cpp/Item.h/main.cpp et makefile

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "item.h"
+
+int main() {
+    Item item(12.0); // Objet "Item" créé avec un prix initial de 12€
+
+    std::cout << "Prix actuel : " << item.getCurrentPrice() << std::endl;
+
+    item.updatePrice(15.0); // Met à jour le prix
+    std::cout << "Nouveau prix : " << item.getCurrentPrice() << std::endl;
+
+    return 0;
+}
+
