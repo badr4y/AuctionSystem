c054c3b2bc41ed6a2f6506875ef1c60e6430c255
Adelite25 <mahmoud.mokrane25@gmail.com>
2023-10-20 16:55:44 +0200

    implementation of the methodes attach(), detach() & notify() in Item files

--- Item.cpp
+++ Item.cpp
@@ -1,6 +1,6 @@
 #include "Item.h"
 #include "Auction.h"
-#include "IBuyer.h"
+#include "Buyer.h"
 
 
 // Constructor
@@ -16,3 +16,17 @@ float Item::getCurrentPrice() const {
     return currentPrice;
 }
 
+void Item::attach(Buyer buyer){
+    buyers.push_back(buyer);
+}
+
+void Item::detach(Buyer buyer){
+    buyers.remove(buyer)
+}
+
+void Item::notify(){
+    for (auto& buyer : buyers){
+	buyer.updatePrice(this.currentPrice);
+    }
+
+}
--- Item.h
+++ Item.h
@@ -2,6 +2,8 @@
 #define ITEM_H
 
 #include "Auction.h"
+#include "Buyer.h"
+#include <list>
 
 class Item :public Auction {
 public:
@@ -12,8 +14,13 @@ public:
 
     float getCurrentPrice() const;
 
+    void attach(Buyer buyer);
+    void detach(Buyer buyer);
+    void notify();
+
 private:
     float currentPrice;
+    std::list<IBuyer> buyers;
 };
 
 #endif // ITEM_H
